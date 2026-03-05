8d7b45455a401ac3f5fbfd2fe1fc5622a23e0ffa
brahmika <brahmika30@gmail.com>
2026-03-05 23:38:34 +0530

    [Brahmika] Added: UC1: Implemented AddressBook Table.

---
+++ UC2.sql
@@ -0,0 +1,16 @@
+-- CREATE database AddressBookService;
+-- show databases;
+-- USE AddressBookService;
+
+create table AddressBook (
+                             first_name varchar(50),
+                             last_name varchar(50),
+                             address varchar(150),
+                             city varchar(50),
+                             state varchar(50),
+                             zip varchar(50),
+                             phone_number varchar(50),
+                             email varchar(100)
+);
+
+SHOW tables;
